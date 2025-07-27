85608ecc30ffe1ff6404a00ddf4ccbb7249e0e3d
GavinLang <ngmsr3494@163.com>
2025-07-27 17:04:12 +0800

    订单支付功能代码

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -19,4 +19,12 @@ public interface UserMapper {
      * @param user
      */
     void insert(User user);
+
+    /**
+     * 根据user_id查询用户
+     * @param userId
+     * @return
+     */
+    @Select("select * from user where id = #{user_id}")
+    User getById(Long userId);
 }
