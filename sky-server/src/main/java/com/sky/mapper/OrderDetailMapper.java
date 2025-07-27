138b1bcd01e6891e9a9812e7445e4c0c6adce990
GavinLang <ngmsr3494@163.com>
2025-07-27 14:49:17 +0800

    用户下单功能代码开发

---
+++ sky-server/src/main/java/com/sky/mapper/OrderDetailMapper.java
@@ -0,0 +1,21 @@
+package com.sky.mapper;
+
+import com.sky.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface OrderDetailMapper {
+    /**
+     * 单条插入订单详情数据
+     * @param orderDetail
+     */
+    void insert(OrderDetail orderDetail);
+
+    /**
+     * 批量插入订单详情数据
+     * @param orderDetailList
+     */
+    void insertBatch(List<OrderDetail> orderDetailList);
+}
